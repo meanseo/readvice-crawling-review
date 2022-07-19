3587fc57e485b77d2c7a2d2298ddb8e8f40668fb
meanseo <chlalstjok97@gmail.com>
2022-07-19 10:40:35 +0900

    save_test

--- remove.py
+++
@@ -1,5 +0,0 @@
-import re
-
-def remove_html(sentence) :
-	sentence = re.sub('(<([^>]+)>)|/n|/t|&nbsp;', '', sentence)
-    return sentence
--- yes24.py
+++ yes24.py
@@ -7,11 +7,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import urllib.request
 import pandas as pd
-
-
 import time
 
-import urllib3
 import re
 
 def remove_html(sentence) :
@@ -29,19 +26,15 @@ driver.get(url)
 경제/경영 3, 만화/라이트노벨 6, 소설/시/희곡 8, 어린이 10, 에세이 11, 여행 12, 
 유아 15, 자기계발 18, IT 모바일 24
 '''
-categoreis = [18, 24]
-review_list3 = []
-review_list6 = []
-review_list8 = []
-review_list10 = []
-review_list11 = []
+categoreis = [1]
+review_list = []
 
 # 장르 클릭
 for category in categoreis:
     driver.find_element(By.XPATH,f'/html/body/div/div[2]/div[1]/div[1]/ul/li[1]/ul/li[{category}]/a').click()      
     time.sleep(2)
     # 페이지 클릭
-    for page in range(1, 4):
+    for page in range(1, 6):
         driver.find_element(By.XPATH,f'//*[@id="bestList"]/div[3]/div[1]/div[1]/p/a[{page}]').click()                                   
         time.sleep(2)
         # 도서 클릭
@@ -56,9 +49,14 @@ for category in categoreis:
                     review = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, f'//*[@id="infoset_reviewContentList"]/div[{i}]/div[3]/div[2]'))).get_attribute("innerHTML")
                     # review = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, f'//*[@id="infoset_oneCommentList"]/div[3]/div[{i}]/div[1]/div[2]/span'))).get_attribute("innerHTML")
                     sentence = remove_html(review)
-                    review_listi .append(sentence)
+                    review_list.append(sentence)
+                    review_pd = pd.DataFrame(review_list)
                     print(f'{review_list}\n')
                 except:
                     continue
             driver.back()
-    pd.DataFrame(review_list).to_csv(f'save/{category}_reivews.csv', index=False)
+    pd.DataFrame(review_list).to_csv(f'save/{category}_reivews.csv', index=True)
+    pd.DataFrame(review_list).to_csv(f'save/{category}_reivews_text.txt', sep = '\t', index=True)
+    review_pd.to_csv(f'save/{category}_reivews_test.csv', index=True)
+pd.DataFrame(review_list).to_csv(f'save/{category}_reivews_.csv', index=False)
+pd.DataFrame(review_list).to_csv(f'save/{category}_reivews_text_.txt', sep = '\t', index=False)
